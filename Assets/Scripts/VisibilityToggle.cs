7ded79fd1c703a81b97378a64708c31ece0c7413
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2025-11-11 10:34:51 +0200

    Update VisibilityToggle.cs

--- Assets/Scripts/VisibilityToggle.cs
+++ Assets/Scripts/VisibilityToggle.cs
@@ -4,35 +4,26 @@ using UnityEngine.InputSystem;
 public class VisibilityToggle : MonoBehaviour
 {
     [Header("Options")]
-    [SerializeField] private bool includeChildren = false;   // also hide children
-    [SerializeField] private bool alsoAffectColliders = false; // also hide colliders
-
+    [SerializeField] private bool includeChildren = false;
+    [SerializeField] private bool alsoAffectColliders = false;
     private Controls controls;          
     private Renderer[] renderers;
     private Collider[] colliders;
     private bool isVisible = true;
-
     void Awake()
     {
-        // get renderers (also children if needed)
         renderers = includeChildren
             ? GetComponentsInChildren<Renderer>(true)
             : new Renderer[] { GetComponent<Renderer>() };
-
-        // get colliders if enabled
         if (alsoAffectColliders)
             colliders = includeChildren
                 ? GetComponentsInChildren<Collider>(true)
                 : new Collider[] { GetComponent<Collider>() };
-
-        // setup input
         controls = new Controls();
         controls.Player.ToggleVisibility.performed += _ => SetVisible(!isVisible);
     }
-
     void OnEnable()  => controls.Player.Enable();
     void OnDisable() => controls.Player.Disable();
-
     private void SetVisible(bool v)
     {
         isVisible = v;
@@ -41,4 +32,3 @@ public class VisibilityToggle : MonoBehaviour
             foreach (var c in colliders) if (c) c.enabled = v;
     }
 }
-
