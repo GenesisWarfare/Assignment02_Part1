6209c36e7fa81d0b32bdd4d0c85c2000a46e73dd
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2025-11-11 10:53:44 +0200

    Update Rotator.cs

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+
 public class Rotator : MonoBehaviour
 {
-  [Header("Rotation speed (degrees per second)")]
-  [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 90f, 0f);
-  [Header("Rotation space")]
-  [SerializeField] private Space rotationSpace = Space.Self;
-  void Update()
-  {
-    transform.Rotate(rotationSpeed * Time.deltaTime, rotationSpace);
-  }
+    [Header("Rotation speed (degrees per second)")]
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 90f, 0f);
+
+    [Header("Rotation space")]
+    [SerializeField] private Space rotationSpace = Space.Self;
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, rotationSpace);
+    }
 }
