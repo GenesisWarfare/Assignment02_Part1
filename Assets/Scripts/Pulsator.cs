8c1ecaa3f369cb2af4ed0160bd997071fa9f28ed
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2025-11-11 10:33:39 +0200

    Update Pulsator.cs

--- Assets/Scripts/Pulsator.cs
+++ Assets/Scripts/Pulsator.cs
@@ -1,26 +1,18 @@
 using UnityEngine;
-
 public class Scaler : MonoBehaviour
 {
     [Header("Amplitude (scale variation)")]
     [SerializeField] private float scaleAmplitude = 0.3f;
-
     [Header("Frequency (beats per second)")]
     [SerializeField] private float frequency = 1f;
-
     private Vector3 baseScale;
-
     void Awake()
     {
-        baseScale = transform.localScale; // initial size
+        baseScale = transform.localScale;
     }
-
     void Update()
     {
-        // Use trigo sinus
         float scaleFactor = 1f + scaleAmplitude * Mathf.Sin(2f * Mathf.PI * frequency * Time.time);
-
-        
         transform.localScale = baseScale * scaleFactor;
     }
 }
