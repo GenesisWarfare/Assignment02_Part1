93ff6369b84d9efa2dad983991c4fa4738110902
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2025-11-11 10:32:26 +0200

    Update Oscillator.cs

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -1,22 +1,17 @@
-
 using UnityEngine;
-
 public class Oscillator : MonoBehaviour
 {
     [SerializeField] float length = 3f;        
     [SerializeField] float maxAngle = 45f;     
     [SerializeField] float frequency = 0.5f;   
     [SerializeField] Vector3 axis = Vector3.forward;  
-
     Vector3 pivotPos;
     Vector3 upDir;
-
     void Awake()
     {
         upDir = Quaternion.FromToRotation(Vector3.forward, axis.normalized) * Vector3.up;
         pivotPos = transform.position + upDir * length;
     }
-
     void Update()
     {
         float angle = maxAngle * Mathf.Sin(2f * Mathf.PI * frequency * Time.time);
